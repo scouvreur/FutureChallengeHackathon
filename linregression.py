f43e96de06b1f55031be2ae140722e7f25b544e1
Stephane Couvreur <stephane.couvreur.sueron@gmail.com>
2018-02-07 01:54:59 +0000

    Lasso regression model gets 94.40% accuracy on wind prediction and 70.25% accuracy on rainfall prediction

--- linregression.py
+++ linregression.py
@@ -13,55 +13,93 @@ def readData():
     '''
     # read h5 format back to numpy array
     global citydata
-    global train
-    global test
+    global train_wind
+    global train_rain
+    global test_wind
+    global test_rain
     h5f = h5py.File('METdata.h5', 'r')
-    # citydata = h5f['citydata'][:]
-    train = h5f['train'][:]
-    test = h5f['test'][:]
+    citydata = h5f['citydata'][:]
+    train_wind = h5f['train_wind'][:]
+    train_rain = h5f['train_rain'][:]
+    test_wind = h5f['test_wind'][:]
+    test_rain = h5f['test_rain'][:]
+    print("--- Data read in successfully ---")
     h5f.close()
 
 readData()
 
-model_MAE = {}
+wind_model_MAE = {}
 for i in range(10):
-	model_MAE[i] = mean_absolute_error(train[:,:,i,:,:].flatten(),
-									   train[:,:,10,:,:].flatten())
-	print(model_MAE[i])
+	wind_model_MAE[i] = mean_absolute_error(train_wind[:,:,i,:,:].flatten(),
+									   train_wind[:,:,10,:,:].flatten())
 
-min_model_key = min(model_MAE, key=model_MAE.get)
-min_model_MAE = 1-(min(model_MAE.values())/train[:,:,10,:,:].mean())
+min_wind_model_key = min(wind_model_MAE, key=wind_model_MAE.get)
+min_wind_model_MAE = 1-(min(wind_model_MAE.values())/train_wind[:,:,10,:,:].mean())
 
-print('Model {} on average performs best with an accuracy of {:0.2f}%.'.format(min_model_key,100*min_model_MAE))
+rain_model_MAE = {}
+for i in range(10):
+    rain_model_MAE[i] = mean_absolute_error(train_rain[:,:,i,:,:].flatten(),
+                                       train_rain[:,:,10,:,:].flatten())
 
-# Taken the average of 10 models as prediction value
+min_rain_model_key = min(rain_model_MAE, key=rain_model_MAE.get)
+min_rain_model_MAE = 1-(min(rain_model_MAE.values())/train_rain[:,:,10,:,:].mean())
 
-train_set = train[:3,:,:10,:,:]
-valid_set = train[3:,:,:10,:,:]
-train_label = train[:3,:,10,:,:]
-valid_label = train[3:,:,10,:,:]
+print('Model {} on average performs best on wind prediction with an accuracy of {:0.2f}%.'.format(min_wind_model_key,100*min_wind_model_MAE))
+print('Model {} on average performs best on rainfall prediction with an accuracy of {:0.2f}%.'.format(min_rain_model_key,100*min_rain_model_MAE))
 
-test_set = test[:,:,:,:,:]
+train_set = train_wind[:3,:,:10,:,:]
+valid_set = train_wind[3:,:,:10,:,:]
+train_label = train_wind[:3,:,10,:,:]
+valid_label = train_wind[3:,:,10,:,:]
+
+test_set = test_wind
 
 # Linear regression model
 
-train_set_1 = np.column_stack((train_set[:,:,i,:,:].flatten() for i in range(10)))
-valid_set_1 = np.column_stack((valid_set[:,:,i,:,:].flatten() for i in range(10)))
-test_set_1 = np.column_stack((test_set[:,:,i,:,:].flatten() for i in range(10)))
+train_set = np.column_stack((train_set[:,:,i,:,:].flatten() for i in range(10)))
+valid_set = np.column_stack((valid_set[:,:,i,:,:].flatten() for i in range(10)))
+test_set = np.column_stack((test_set[:,:,i,:,:].flatten() for i in range(10)))
 
-train_label_1 = train_label.flatten()
-valid_label_1 = valid_label.flatten()
+train_label = train_label.flatten()
+valid_label = valid_label.flatten()
 
 lr = LinearRegression(fit_intercept=True, normalize=True)
-lr.fit(train_set_1,train_label_1)
-linreg_MAE = mean_absolute_error(lr.predict(valid_set_1),valid_label_1)
-linreg_percent_error = 1 - (linreg_MAE/train[:,:,10,:,:].mean())
+lr.fit(train_set,train_label)
+linreg_MAE = mean_absolute_error(lr.predict(valid_set),valid_label)
+linreg_percent_error = 1 - (linreg_MAE/train_wind[:,:,10,:,:].mean())
 
-print('The linear regression model has an accuracy of {:0.2f}%.'.format(100*linreg_percent_error))
+print('The linear regression model has an accuracy of {:0.2f}% on wind prediction.'.format(100*linreg_percent_error))
 
-linreg = lr.predict(test_set_1)
-linreg = linreg.reshape(5,18,548,421)
+linreg_wind = lr.predict(test_set)
+linreg_wind = linreg_wind.reshape(5,18,548,421)
+
+train_set = train_rain[:3,:,:10,:,:]
+valid_set = train_rain[3:,:,:10,:,:]
+train_label = train_rain[:3,:,10,:,:]
+valid_label = train_rain[3:,:,10,:,:]
+
+test_set = test_rain
+
+# Linear regression model
+
+train_set = np.column_stack((train_set[:,:,i,:,:].flatten() for i in range(10)))
+valid_set = np.column_stack((valid_set[:,:,i,:,:].flatten() for i in range(10)))
+test_set = np.column_stack((test_set[:,:,i,:,:].flatten() for i in range(10)))
+
+train_label = train_label.flatten()
+valid_label = valid_label.flatten()
+
+lr = LinearRegression(fit_intercept=True, normalize=True)
+lr.fit(train_set,train_label)
+linreg_MAE = mean_absolute_error(lr.predict(valid_set),valid_label)
+linreg_percent_error = 1 - (linreg_MAE/train_rain[:,:,10,:,:].mean())
+
+print('The linear regression model has an accuracy of {:0.2f}% on rainfall prediction.'.format(100*linreg_percent_error))
+
+linreg_rain = lr.predict(test_set)
+linreg_rain = linreg_rain.reshape(5,18,548,421)
 
 h5f = h5py.File('LinReg.h5', 'w')
-h5f.create_dataset('linreg', data=linreg)
+h5f.create_dataset('linreg_wind', data=linreg_wind)
+h5f.create_dataset('linreg_rain', data=linreg_rain)
 h5f.close()
